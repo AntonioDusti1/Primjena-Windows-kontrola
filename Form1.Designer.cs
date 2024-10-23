244f28fb29668513ab859bd1f2f2a90df7357bf9
AntonioDusti1 <152974953+AntonioDusti1@users.noreply.github.com>
2024-10-23 09:22:05 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace Primjena_Windows_kontrola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxPrograms = new System.Windows.Forms.ComboBox();
+            this.checkBoxShowProgramAction = new System.Windows.Forms.CheckBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.groupBoxRunProgram = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBoxProgramName = new System.Windows.Forms.TextBox();
+            this.textBoxProgramDescription = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem7 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem8 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem9 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem10 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem11 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem12 = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBoxRunProgram.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBoxPrograms
+            // 
+            this.comboBoxPrograms.FormattingEnabled = true;
+            this.comboBoxPrograms.Location = new System.Drawing.Point(6, 72);
+            this.comboBoxPrograms.Name = "comboBoxPrograms";
+            this.comboBoxPrograms.Size = new System.Drawing.Size(196, 28);
+            this.comboBoxPrograms.TabIndex = 0;
+            // 
+            // checkBoxShowProgramAction
+            // 
+            this.checkBoxShowProgramAction.AutoSize = true;
+            this.checkBoxShowProgramAction.Location = new System.Drawing.Point(33, 163);
+            this.checkBoxShowProgramAction.Name = "checkBoxShowProgramAction";
+            this.checkBoxShowProgramAction.Size = new System.Drawing.Size(185, 24);
+            this.checkBoxShowProgramAction.TabIndex = 1;
+            this.checkBoxShowProgramAction.Text = "Show program action";
+            this.checkBoxShowProgramAction.UseVisualStyleBackColor = true;
+            this.checkBoxShowProgramAction.CheckedChanged += new System.EventHandler(this.checkBoxShowProgramAction_CheckedChanged);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(251, 72);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(115, 36);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "Run program";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // groupBoxRunProgram
+            // 
+            this.groupBoxRunProgram.Controls.Add(this.btnRun);
+            this.groupBoxRunProgram.Controls.Add(this.comboBoxPrograms);
+            this.groupBoxRunProgram.Location = new System.Drawing.Point(33, 38);
+            this.groupBoxRunProgram.Name = "groupBoxRunProgram";
+            this.groupBoxRunProgram.Size = new System.Drawing.Size(372, 110);
+            this.groupBoxRunProgram.TabIndex = 3;
+            this.groupBoxRunProgram.TabStop = false;
+            this.groupBoxRunProgram.Text = "Run a progam";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.textBoxProgramDescription);
+            this.groupBox2.Controls.Add(this.textBoxProgramName);
+            this.groupBox2.Location = new System.Drawing.Point(33, 193);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(372, 245);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Program control";
+            // 
+            // textBoxProgramName
+            // 
+            this.textBoxProgramName.Location = new System.Drawing.Point(6, 51);
+            this.textBoxProgramName.Name = "textBoxProgramName";
+            this.textBoxProgramName.Size = new System.Drawing.Size(175, 26);
+            this.textBoxProgramName.TabIndex = 0;
+            // 
+            // textBoxProgramDescription
+            // 
+            this.textBoxProgramDescription.Location = new System.Drawing.Point(7, 111);
+            this.textBoxProgramDescription.Multiline = true;
+            this.textBoxProgramDescription.Name = "textBoxProgramDescription";
+            this.textBoxProgramDescription.Size = new System.Drawing.Size(226, 106);
+            this.textBoxProgramDescription.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(251, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Program name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Program descripition";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem5,
+            this.toolStripMenuItem9,
+            this.toolStripMenuItem11});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(54, 29);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(270, 34);
+            this.toolStripMenuItem2.Text = "Open";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(270, 34);
+            this.toolStripMenuItem3.Text = "Save";
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(270, 34);
+            this.toolStripMenuItem4.Text = "Exit";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem6,
+            this.toolStripMenuItem7,
+            this.toolStripMenuItem8});
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(58, 29);
+            this.toolStripMenuItem5.Text = "Edit";
+            // 
+            // toolStripMenuItem6
+            // 
+            this.toolStripMenuItem6.Name = "toolStripMenuItem6";
+            this.toolStripMenuItem6.Size = new System.Drawing.Size(270, 34);
+            this.toolStripMenuItem6.Text = "Text";
+            this.toolStripMenuItem6.Click += new System.EventHandler(this.toolStripMenuItem6_Click);
+            // 
+            // toolStripMenuItem7
+            // 
+            this.toolStripMenuItem7.Name = "toolStripMenuItem7";
+            this.toolStripMenuItem7.Size = new System.Drawing.Size(270, 34);
+            this.toolStripMenuItem7.Text = "Font";
+            // 
+            // toolStripMenuItem8
+            // 
+            this.toolStripMenuItem8.Name = "toolStripMenuItem8";
+            this.toolStripMenuItem8.Size = new System.Drawing.Size(270, 34);
+            this.toolStripMenuItem8.Text = "Options";
+            // 
+            // toolStripMenuItem9
+            // 
+            this.toolStripMenuItem9.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem10});
+            this.toolStripMenuItem9.Name = "toolStripMenuItem9";
+            this.toolStripMenuItem9.Size = new System.Drawing.Size(65, 29);
+            this.toolStripMenuItem9.Text = "View";
+            // 
+            // toolStripMenuItem10
+            // 
+            this.toolStripMenuItem10.Name = "toolStripMenuItem10";
+            this.toolStripMenuItem10.Size = new System.Drawing.Size(270, 34);
+            this.toolStripMenuItem10.Text = "Message";
+            this.toolStripMenuItem10.Click += new System.EventHandler(this.toolStripMenuItem10_Click);
+            // 
+            // toolStripMenuItem11
+            // 
+            this.toolStripMenuItem11.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem12});
+            this.toolStripMenuItem11.Name = "toolStripMenuItem11";
+            this.toolStripMenuItem11.Size = new System.Drawing.Size(65, 29);
+            this.toolStripMenuItem11.Text = "Help";
+            // 
+            // toolStripMenuItem12
+            // 
+            this.toolStripMenuItem12.Name = "toolStripMenuItem12";
+            this.toolStripMenuItem12.Size = new System.Drawing.Size(270, 34);
+            this.toolStripMenuItem12.Text = "About";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBoxRunProgram);
+            this.Controls.Add(this.checkBoxShowProgramAction);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBoxRunProgram.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxPrograms;
+        private System.Windows.Forms.CheckBox checkBoxShowProgramAction;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.GroupBox groupBoxRunProgram;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxProgramDescription;
+        private System.Windows.Forms.TextBox textBoxProgramName;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem7;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem8;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem9;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem10;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem11;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem12;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Primjena_Windows_kontrola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            string selectedProgram = comboBoxPrograms.SelectedItem.ToString();
+            switch (selectedProgram)
+            {
+                case "Notepad":
+                    System.Diagnostics.Process.Start("notepad.exe");
+                    break;
+                case "Paint":
+                    System.Diagnostics.Process.Start("mspaint.exe");
+                    break;
+            }
+        }
+
+        private void checkBoxShowProgramAction_CheckedChanged(object sender, EventArgs e)
+        {
+            groupBoxRunProgram.Visible = checkBoxShowProgramAction.Checked;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBoxProgramName.Clear();
+            textBoxProgramDescription.Clear();
+        }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Želite li izaći?", "Potvrda izlaza", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void toolStripMenuItem10_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ovo je informacijska poruka.", "Informacija", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
+        }
+
+        private void toolStripMenuItem6_Click(object sender, EventArgs e)
+        {
+            FormaText FormaText = new FormaText();
+            FormaText.ShowDialog();
+            textBoxProgramName.Text = FormaText.textBox1;
+            textBoxProgramDescription.Text = FormaText.textBox2;
+        }
+    }
+}
